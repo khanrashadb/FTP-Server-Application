2980ff09070de97c48d0255711a551866aacd80a
Rashad Khan <khanr@stumail.csci.viu.ca>
2023-03-21 21:20:43 -0700

    Working on startClientSession function

--- src/ftp_server_session.cpp
+++ src/ftp_server_session.cpp
@@ -12,13 +12,44 @@
 #include <stdlib.h>
 #include <string>
 #include <string.h>
+#include <unistd.h>
 #include "ftp_server_session.h"
+#include "ftp_server_connection.h"
+#include "ftp_server_response.h"
 
 using namespace std;
 
 void startClientFTPSession(ClientFtpSession& clientFtpSession)
-{
+{   
+    char path[256];
+    size_t pathLength = sizeof(path);
+    bool isReady = false;
+    bool isError = false;
+    bool isTimedout = false;
+    int status = sendToRemote(clientFtpSession.controlConnection, CONNECTED_RESPONSE, sizeof(CONNECTED_RESPONSE));
+    
+    if(status < 0)
 
+        cout << "Encountred an error while sending data\n";
+
+    clientFtpSession.isControlConnected = true;
+
+    int bytesReturned = readlink("/proc/self/exe", path, pathLength);
+    
+    if(bytesReturned > 0)
+    {
+        path[bytesReturned] = '\0';
+        strcpy(clientFtpSession.rootDir, path);
+    }
+
+    while(clientFtpSession.isControlConnected)
+    {
+        isReady = isConnectionReadyToRead(clientFtpSession.controlConnection, FTP_CLIENT_SESSION_TIMEOUT_SEC, FTP_CLIENT_SESSION_TIMEOUT_USEC, isError, isTimedout);
+
+        if(isReady == false)
+
+            cout << "Error! Connection is not reading\n";
+    }   
 }
 
 void stopClientFTPSession(ClientFtpSession& clientFtpSession)
